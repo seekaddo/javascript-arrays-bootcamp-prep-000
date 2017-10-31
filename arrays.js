532795d8bedb90f89fc2d09528948033eec042d3
Dennis Addo <dennis_addo@aol.com>
2017-10-31 15:44:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,26 @@
 
+<<<<<<< HEAD
 
  var chocolateBars = [ "snickers","hundred grand", "kitkat","skittles" ];
 
  function addElementToBeginningOfArray(array,value) {
    return [value,...array];
+=======
+ let chocolateBars = [
+   "snickers",
+   "hundred grand",
+   "kitkat",
+   "skittles"
+ ];
+
+ function addElementToBeginningOfArray(array,value) {
+   return [value,... array);
+>>>>>>> a57818f5c74c3266a8ceb22eac53eefcf648ff4e
  }
 
 
  function destructivelyAddElementToBeginningOfArray(array,value) {
+<<<<<<< HEAD
     array.unshift(value); //this mutate the elements in the array
     return array;
  }
@@ -21,6 +34,17 @@ function destructivelyAddElementToEndOfArray(array,elem) {
    array.push(elem);
    return array;
 
+=======
+   return array.unshift(value); //this mutate the elements in the array
+ }
+
+function addElementToEndOfArray(array, elem) {
+  return [array,...elem];
+}
+
+function destructivelyAddElementToEndOfArray(array,elem) {
+  return array.push(elem);
+>>>>>>> a57818f5c74c3266a8ceb22eac53eefcf648ff4e
 }
 
 function accessElementInArray(array, index) {
